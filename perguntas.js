c96fff04a2356f97d2c6cd728683119df2d98107
pedro! <00001072554616sp@al.educacao.sp.gov.br>
2024-11-06 18:06:41 -0300

    projeto final

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Indústria musical',
+    'Qual foi o primeiro álbum lançado por Taylor Swift?',
+    '"Taylor Swift", em 2006, com um estilo voltado para o country e com canções autorais sobre sua adolescência.'
+)
+
+criaCartao(
+    'Indústria musical',
+    'Qual música impulsionou Taylor Swift ao estrelato?',
+    '"Love Story", lançada em 2008 no álbum Fearless. A música popularizou Taylor mundialmente, tornando-se um dos maiores sucessos de sua carreira.'
+)
+
+criaCartao(
+    'Indústria musical',
+    'Quantos Grammys Taylor Swift já ganhou até agora?',
+    'Até 2024, Taylor já ganhou 14 prêmios Grammys, incluindo a categoria mais prestigiada de Álbum do Ano em quatro ocasiões, com o Fearless, 1989, Folklore e Midnights.'
+)
+
+criaCartao(
+    'Indústria musical',
+    'Qual foi a transição de estilo musical que Taylor Swift fez em sua carreira?',
+    'Taylor começou com um estilo country e, a partir do álbum 1989, fez um transição para o pop, que marcou sua carreira com um grande sucesso crítico e comercial.'
+)
+
+criaCartao(
+    'Indústria musical',
+    'Por que Taylor Swift está regravando seus primeiros álbuns?',
+    'Taylor teve que regravar seus seis primeiros álbuns para recuperar os direitos sobre suas músicas após uma disputa com sua antiga gravadora, Big Machine Records.'
+)
+
+criaCartao(
+    'Indústria musical',
+    'O que é o "Taylor Swift: The Eras Tour"?',
+    'É uma turnê lançada em 2023 que celebra a carreira de Taylor, com uma setlist diversa de suas músicas, cenários representando seus álbuns e mais de 10 peças de roupa.'
+)
